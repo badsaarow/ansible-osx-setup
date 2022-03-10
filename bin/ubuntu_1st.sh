9352e5fa34bedc3a71219d3e2797b0893a1c348b
sungyong <sungyong@gmail.com>
2022-03-10 10:56:15 +0900

    feat: add 1st script for ubuntu

---
+++ bin/ubuntu_1st.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt -y update
+sudo apt -y install curl wget git zsh build-essential ca-certificates gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt -y update
+sudo apt -y install docker-ce docker-ce-cli containerd.io python-is-python3 pip docker-compose
+sudo pip install --ignore-installed ansible
+
+git clone git@github.com:badsaarow/ansible-osx-setup.git
+cd ansible-osx-setup || exit
+
+ansible-galaxy install -r requirements.yml
