0b3ec2a63e84e3cfcbf40871d01265f9208e1025
sungyong <sungyong@gmail.com>
2022-04-30 14:41:23 +0900

    fix: execution path

--- bin/ubuntu_3rd_cli.sh
+++ bin/ubuntu_3rd_cli.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/4th_zsh.yaml --ask-become-pass
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/5th_brew.yaml --ask-become-pass
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/6th_brew_env.yaml --ask-become-pass
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/7th_kubernetes.yaml --ask-become-pass
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/8th_nodejs.yaml --ask-become-pass
-ansible-playbook -i "localhost," -c local ../ansible_ubuntu/9th_python.yaml --ask-become-pass
+cd $HOME/ansible-osx-setup/ansible_ubuntu
+ansible-playbook -i "localhost," -c local 4th_zsh.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local 5th_brew.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local 6th_brew_env.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local 7th_kubernetes.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local 8th_nodejs.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local 9th_python.yaml --ask-become-pass
