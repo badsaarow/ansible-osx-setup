fe661a81878f1c9e77ad4b4b5ed005a5a40cf06f
sungyong <sungyong@gmail.com>
2022-06-10 11:23:16 +0900

    feat: add zoxide prettyping

--- bin/ubuntu_3rd_cli.sh
+++ bin/ubuntu_3rd_cli.sh
@@ -8,7 +8,12 @@ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zprofile
 source ~/.zprofile
 brew install gcc docker-compose python golang adr-tools tig nodejs yarn 
 brew install most bpytop duf jq yq gtop tldr fx bat duf dust
+brew install curlie httpie procs cheat dog mcfly ripgrep fd broot duf dust delta exa bat zoxide ripgrep 
 # kind kubernetes-cli helm
 brew update --force --quiet
+
+wget https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping
+chmod +x prettyping
+mv prettyping ~/bin/
 
 ansible-playbook -i "localhost," -c local 9th_python.yaml --ask-become-pass
