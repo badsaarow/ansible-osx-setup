7a1b02ce014b1a403b9bdfc3569fe4d816fcae85
sungyong <sungyong@gmail.com>
2023-08-04 16:01:56 +0900

    fix: ubuntu

--- bin/ubuntu_1st.sh
+++ bin/ubuntu_1st.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 sudo apt -y update
-sudo apt -y install curl wget vim git zsh build-essential ca-certificates gnupg lsb-release net-tools network-manager
+sudo apt -y install curl wget vim git tig build-essential ca-certificates gnupg lsb-release net-tools network-manager
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt -y update
 sudo apt -y install docker-ce docker-ce-cli containerd.io python-is-python3 pip docker-compose
-sudo pip install --ignore-installed ansible
 
+## brew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+source ~/.profile
+
+
+## ansible
+sudo pip install --ignore-installed ansible
 git clone https://github.com/badsaarow/ansible-osx-setup.git
 cd ansible-osx-setup || exit
 
