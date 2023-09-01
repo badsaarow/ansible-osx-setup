77c79cd5911500bddbacf9a046407b033ad92524
sungyong <sungyong@gmail.com>
2023-09-01 10:15:06 +0900

    chore: separate brew for arm

--- bin/ubuntu_1st.sh
+++ bin/ubuntu_1st.sh
@@ -5,7 +5,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --y
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt -y update
 sudo apt -y install docker-ce docker-ce-cli containerd.io python-is-python3 pip docker-compose
-
-## brew
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
---
+++ bin/ubuntu_brew.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+## brew
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
